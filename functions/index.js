d2588a7a3fa2ee7bb443c540088a7f78f43af633
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2025-11-27 11:49:03 +0000

    Brought back tracing

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,7 @@ import MailComposer from "nodemailer/lib/mail-composer/index.js";
 // Initializations
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
   enableLogs: true,
   integrations: [Sentry.googleGenAIIntegration({
     recordInputs: true,
